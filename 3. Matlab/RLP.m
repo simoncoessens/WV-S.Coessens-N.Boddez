3e72f64c4d5a83f561b3bf03432247f3c70f8d0e
Noe Boddez <n.boddez1@hotmail.com>
2022-02-21 19:40:49 +0100

    code efficienter gemaakt en andere aanpassingen

--- 3. Matlab/RLP.m
+++ 3. Matlab/RLP.m
@@ -1,21 +1,16 @@
 function [x] = RLP(xp, y, A, eps)
-    x = zeros(length(xp), 1);
-    I = zeros(1);
-    idx = 1;
-    for i = 1: length(y)
-        if y(i) == 1
-            I(idx) = i;
-            idx = idx + 1;
-        end
-    end
+    n = length(xp);
+    x = zeros(n,1);
 
-    for l = 1:ceil(log(length(I)/eps))
-        xl = zeros(size(xp));
-        for i = 1 : length(xp)
+    % length(I) = norm(y,1) dus moeten we I niet eens berekenen
+
+    for l = 1:ceil(log(norm(y,1)/eps))
+        xl = zeros(n,1);
+        for i = 1 : n
             xl(i) = rand(1,1) < xp(i);
         end
         x = x | xl;
-        if boolMatrixMult(A, xl) == y
+        if logical(A*xl) == y
             return
         end
     end
--- 3. Matlab/grouptesting.m
+++ 3. Matlab/grouptesting.m
@@ -14,7 +14,7 @@ for i = 1:k
    result(pos_idx(i)) = 1;
 end
 
-% A = Matrix containing lineair combinations of samples
+% A = Measurement matrix containing lineair combinations of samples
 % m = measurement size
 % p = P(A_i,j == 1) = de kans dat een individu in een test zit
 p = 1/k;
@@ -23,8 +23,7 @@ A = double(rand(m,n) < p);
 
 
 % b = undersampled measurement, b = A v result
-b = boolMatrixMult(A, result);
-
+b = double(logical(A*result));
 
 % b = undersampled measurement
 
@@ -51,32 +50,8 @@ y = pinv(A)*b;
 x = l1eq_pd(y,A,A',b,5e-3,32);
 
 % Random Lineair Programming
-x = RLP(x, b, A, 1e-100);
+x = RLP(x, b, A, 1e-5);
 
-result_undersampled = zeros(k,1);
-count = 1;
-for i = 1:n
-    if x(i) == 1
-        result_undersampled(count) = i;
-        count = count + 1;
-    end
-end
-result_undersampled = sort(result_undersampled);
-
-
-% Check exactness of reconstruction
-err = 0;
-for i =1:k
-    if x(pos_idx(i))~=1 
-        err = err +1;
-    end
-end
-
-
-err_p = (1 - err/k)*100;
-
-
-fprintf('The solution is %f percent correct\n',err_p)
 
 % Absolute and relative error of reconstruction (using l1 norm)
 abs_err = norm(result-x, 1);

1b01135c5cb78ccd5632adcd27ed153dc24bec5b
Noe Boddez <n.boddez1@hotmail.com>
2022-04-02 16:29:35 +0200

    nieuwe malioutov tests

--- 3. Matlab/malioutov.m
+++ 3. Matlab/malioutov.m
@@ -4,11 +4,14 @@ numTest = 200; % Het aantal tests waarover we het gemiddelde nemen
 result_linprog = zeros(max_m/step_size, numTest); 
 result_GNU = zeros(max_m/step_size, numTest);
 
+n = 100;
+k = 1;
+
 for m = 1:max_m/step_size
     disp(m);
     for i = 1:numTest
-        result_linprog(m,i) = group_tester(200, 2, m*step_size);
-        result_GNU(m,i) = group_tester_GNU(m*step_size);
+        result_linprog(m,i) = group_tester(n, k, m*step_size);
+        result_GNU(m,i) = group_tester_GNU(n, k, m*step_size);
     end
 end
 

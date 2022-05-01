44c636fa35bdf1dd348897efa5d4bda04cf7ada6
Simon Coessens <simon.coessens@telenet.be>
2022-05-01 11:50:13 +0200

    zip?

--- 3. Matlab/Tests/4. Group - Aantal tests - reconstructie lijn/HeatMap/temp.m
+++ 3. Matlab/Tests/4. Group - Aantal tests - reconstructie lijn/HeatMap/temp.m
@@ -1,22 +1,8 @@
-% naar normale richting brengen, (matlab telt naar beneden)
-result = flipud(result);
-[max_row, max_collumn] = size(result);
 
-% 1tjes vullen
-for i=1:max_collumn
-    for j=1:max_row
-        if result(j,i) > p
-            break;
-        else
-            % perfecte reconstructie boven onze graad van p
-            result(j,i) = 1;
-        end
-    end
-end
 
 % heatmap maken
 n = 1:100:4800;
-m = 1:step_m:max_row;
+m = 1:step_m:(max_row*step_m);
 imagesc(n,m,result)
 colormap default
 colorbar

25f2d55134e259dcc5cae283396a37a43e91878e
Noe Boddez <n.boddez1@hotmail.com>
2022-05-01 14:01:45 +0200

    setup veel sneller

--- 3. Matlab/Tests/1. Groupsize - Time/TestSetupLinprog.m
+++ 3. Matlab/Tests/1. Groupsize - Time/TestSetupLinprog.m
@@ -1,4 +1,4 @@
-function [f, A, b, Aj, bj, lb, ub, result] = TestSetupLinprog(n,m,k_percent)
+function [f, Ai, bi, Aj, bj, lb, ub, result] = TestSetupLinprog(n,m,k_percent)
 
 % aantal positieven
 k = round(n*k_percent);
@@ -31,12 +31,9 @@ f = ones(n,1);
 lb = zeros(n,1);
 ub = ones(n,1);
 
-Aj = zeros(0);
-for i = 1:m
-    if b(i) == 0
-        Aj = [Aj ; A(i,:)];
-    end    
-end
-
-Aj = sparse(Aj);
 bj = zeros(m-norm(b,1),1);
+bi = ones(norm(b,1),1);
+
+% Split A up in Ai and Aj
+Ai = A(logical(b), :);
+Aj = A(logical(not(b)), :);

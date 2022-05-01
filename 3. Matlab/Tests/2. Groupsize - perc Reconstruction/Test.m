c129895819c3d000e29b6d43f1a657efe0dcfa76
Noe Boddez <n.boddez1@hotmail.com>
2022-05-02 00:05:49 +0200

    test setup aangepast

--- 3. Matlab/Tests/2. Groupsize - perc Reconstruction/Test.m
+++ 3. Matlab/Tests/2. Groupsize - perc Reconstruction/Test.m
@@ -4,22 +4,33 @@ function succes = Test(n,m,k,alg)
 
 % Initialize al the necessary variables
 
-if alg == 'lin'
+if alg == "linprog noisless"
     % init linprog vars
     [f, A, b, Aj, bj, lb, ub, result] = TestSetupLinprog(n,m,k);
     options = optimoptions('linprog','Display','none'); 
     x = linprog(f, -A, -b, Aj, bj, lb, ub, options);
     succes = isequal(x, result);
-elseif alg == 'GNU'
+elseif alg == "GNU"
     % init GNU
     [c, A, b, lb, ub, ctype, vartype, sense, param, result] = TestSetupGNU(n,m,k);
     x = glpk(c,A,b,lb,ub,ctype,vartype,sense,param);
     succes = isequal(x, result);
-elseif alg == 'mosek'
+elseif alg == "mosek noisless"
     % init mosek
     [cmd, prob, param, result] = TestSetupMosek(n,m,k);
     [~,res] = mosekopt(cmd,prob,param);
     succes = isequal(res.sol.bas.xx, result);
+elseif alg == "linprog noisy"
+    % init linprog vars
+    [f, A, b, Aj, bj, lb, ub, result] = TestSetupNoisyLinprog(n,m,k);
+    options = optimoptions('linprog','Display','none'); 
+    x = linprog(f, -A, -b, Aj, bj, lb, ub, options);
+    succes = isequal(x(1:n), result);
+elseif alg == "mosek noisy"
+    % init mosek
+    [cmd, prob, param, result] = TestSetupNoisyMosek(n,m,k);
+    [~,res] = mosekopt(cmd,prob,param);
+    succes = isequal(res.sol.bas.xx(1:n), result);
 else
-    disp("parameter 'alg' is verkeerd, probeer 'lin', 'GNU' of 'mosek'");
+    disp("parameter 'alg' is verkeerd, probeer 'linprog noiseless', 'linprog noisy', 'GNU', 'mosek noisless' of 'mosek noisy'");
 end

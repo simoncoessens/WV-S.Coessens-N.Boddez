05aea0e948ed22a33c6a8b99bd788546cfb5940c
simoncoessens <56151036+simoncoessens@users.noreply.github.com>
2022-05-02 23:27:48 +0200

    sorry

--- 3. Matlab/Tests/4. Group - Aantal tests - reconstructie lijn/Results/Plot_result.m
+++ 3. Matlab/Tests/4. Group - Aantal tests - reconstructie lijn/Results/Plot_result.m
@@ -1,11 +1,19 @@
-load('100_9600_linprog_noisy.mat');
+load('100_19100_noiseless.mat');
 
 % gewone plot
-figure;
+f = figure;
+f.Position = [100 100 600 400];
+
+title('Reconstructie per aantal metingen');
+xlabel('Aantal metingen')
+ylabel('Reconstructie percentage')
+
 plot(result(:,1),result(:,2));
+
+set(gca,"FontSize",14);
 
 
 % plot van de percenten
-prctn = result(:,2) ./ result(:,1);
-figure;
-plot(result(:,1),prctn);
+%prctn = result(:,2) ./ result(:,1);
+%figure;
+%plot(result(:,1),prctn);
